a81043c4d9c3627f41efd8b68a8b6167261a69b3
zhukovme <zhukovme@gmail.com>
2015-09-03 16:34:17 +0300

    Initial commit

---
+++ app/src/main/java/me/zhukov/remindme/MainActivity.java
@@ -0,0 +1,31 @@
+package me.zhukov.remindme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
