f6f9bcd58cd6bd2eb152217b128ac5e88dd1ddae
zhukovme <zhukovme@gmail.com>
2015-09-03 17:00:19 +0300

    Add toolbar to MainActivity

--- app/src/main/java/me/zhukov/remindme/MainActivity.java
+++ app/src/main/java/me/zhukov/remindme/MainActivity.java
@@ -2,6 +2,7 @@ package me.zhukov.remindme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
     }
 
     @Override
